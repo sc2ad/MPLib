2c7499be30c4f134df87c792be132b013d853d0d
Sc2ad <adamznow@gmail.com>
2018-01-30 09:46:53 -0800

    Inserted comment - Need to delete unwanted info

--- src/paths/RobotPath.java
+++ src/paths/RobotPath.java
@@ -1,5 +1,41 @@
 package paths;
+/*
+1. You've generated a path using a spline to fit points. My one comment here is your path has a very sharp turn. Is this necessary for your mission? As we've talked about, with no means to directly sense vehicle position, skidding is your enemy, and tight turns equal skidding.
 
+2. Assume your along-path velocity follows a trapezoidal profile. Also assume your time steps are small enough to approximate acceleration and heading as constant between time steps. If this is not the case then the analysis gets more complex, so try this first and see how it works.
+
+I will use the following terminology:
+k = time step index
+dt = duration of a single time step
+v = along-path velocity of vehicle centroid , where "centroid" is the midpoint between wheels.
+s = distance centroid has traveled along path.
+theta = angular position (i.e. heading) of vehicle
+w = angular velocity of vehicle
+alpha = angular acceleration of vehicle
+L = distance between wheels
+r = wheel radius
+v_left, v_right = linear velocity of wheel center
+x_left, y_left, x_right, y_right = coordinate of wheel center
+w_left, w_right = angular velocity of wheel
+alpha_left, alpha_right = angular acceleration of wheel
+p_left, p_right = angular position of wheel
+
+3. Integrate velocity profile to get along-path distance: s(k) = s(k-1) + v(k) * dt
+
+4. Compute heading at current position: theta(k) = derivative_of_spline_at_point( s(k) ).
+
+5. Compute angular velocity. It's possible to calculate given path velocity and curvature, but probably just approximate as w(k) = (theta(k) - theta(k-1))/dt
+
+6. Wheel linear velocities are: v_left(k) = v(k) + w(k) * L/2 and v_right(k) = v(k) - w(k) * L/2
+
+7. Wheel angular velocities are: w_left(k) = v_left(k) / r and w_right(k) =v_right(k) / r
+
+8. Now your going to want to validate your path against the physical capabilities of your vehicle by checking the wheel accelerations at every point on the path. You can approximate the required angular acceleration of a wheel as: alpha_left(k) = (w_left(k) - w_left(k-1)) / dt. And this should be compared against an experimentally determined maximum wheel acceleration that is achievable on a fully loaded vehicle and does not slip the wheels.
+
+8. Integrating wheel velocities to get wheel positions accumulates several earlier approximations, so errors will be greatest here. I think an acceptable result can be achieved with small enough dt.  Assuming you run PID wheel position control at each wheel, your reference angular position at any time step will be: p_left(k) = p_left(k-1) + w_left(k) * dt. This would be compared to encoder position from the left wheel encoder for the error input to the left wheel PID.
+
+9. Finally you want to generate wheel paths to validate your algorithms. x_left(k) = x_left(k-1) + v_left(k) * sin(theta(k)),  y_left(k) = x_left(k-1) + v_left(k) * cos(theta(k))
+ */
 public class RobotPath {
 	Trajectory traj;
 	double[] headings, curvatures;
